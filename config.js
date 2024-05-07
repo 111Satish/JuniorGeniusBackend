cac2600dd3da8a2443fb2424a865f715f3ae9fcc
111Satish <satishjnvr@gmail.com>
2024-05-07 14:56:52 +0530

    Updated

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const uri = `mongodb+srv://satishjnvr1:JuniorGenius@cluster0.plcwh0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = `mongodb://127.0.0.1:27017/JuniorGenius`; 
+const uri = `mongodb+srv://satishjnvr1:JuniorGenius@cluster0.plcwh0h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const uri = `mongodb://127.0.0.1:27017/JuniorGenius`; 
 
 const MdbConnect = async () => {
     try {
